f6a3dd057192e1bf878bc5dec96c74a0a42c91f7
Joe Lee <blueberrymozart@gmail.com>
2017-09-19 15:45:14 -0500

    chore: deploy to heroku when master build succeeds
    [#151158796]

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const router = require('./router')
 
 const app = express()
 app.use('/', router)
-app.set('port', 3000)
+app.set('port', process.env.PORT || 3000)
 
 app.listen(app.get('port'), () => {
   console.log(`App listening on port ${app.get('port')}`)
