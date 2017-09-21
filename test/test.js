dbc05b464f93dfd4bf908b71e4d34cc07853ff1c
Yosef Benny Widyokarsono <fybwid@gmail.com>
2017-09-21 16:32:43 +0000

    Added test

---
+++ test/test.js
@@ -0,0 +1,9 @@
+var assert = require('should-http');
+
+describe('Test root page', function() {
+  describe('#1 Header test', function() {
+    it('Should be HTML', function(){
+      this.should.be.html
+    });
+  });
+});
