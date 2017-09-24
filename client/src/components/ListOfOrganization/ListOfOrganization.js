fbf0db4c028ad8e975e451ce08aa69b11e0a665a
Yugma Moradia <yugma93@gmail.com>
2017-09-24 15:23:26 -0700

    Refactoring code as per code review comments

--- client/src/components/ListOfOrganization/ListOfOrganization.js
+++ client/src/components/ListOfOrganization/ListOfOrganization.js
@@ -1,14 +1,10 @@
-/**
- * Created by ravi on 9/24/17.
- */
+
 import styles from './ListOfOrganization.css'
 import React from 'react'
 
 function ListOfOrganization () {
   return (
-    <form className={styles.form}>
-      <button className={styles.loginButton}>LOGIN</button>
-      <button className={styles.signUpButton}>SIGN UP</button>
+    <section className={styles.orgSection}>
       <h1 className={styles.title}>Apply Below</h1>
       <ul className={styles.listOrg}>
         <li>Name:Organization1 Role:Role1</li>
@@ -17,7 +13,7 @@ function ListOfOrganization () {
         <li>Name:Organization4 Role:Role4</li>
         <li>Name:Organization5 Role:Role5</li>
       </ul>
-    </form>
+    </section>
   )
 }
 
