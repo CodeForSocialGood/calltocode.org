d828fd6185c8deba127f15c77def0e296c2229d9
Karthi Jagatheesan <karthijey@yahoo.com>
2017-09-24 19:57:06 -0400

    added css
    Signed-off-by: Yugma Moradia <yugma93@gmail.com>

--- client/src/components/ListOfOrganization/ListOfOrganization.js
+++ client/src/components/ListOfOrganization/ListOfOrganization.js
@@ -6,12 +6,12 @@ function ListOfOrganization () {
   return (
     <section className={styles.orgSection}>
       <h1 className={styles.title}>Apply Below</h1>
-      <ul className={styles.listOrg}>
-        <li>Name:Organization1 Role:Role1</li>
-        <li>Name:Organization2 Role:Role2</li>
-        <li>Name:Organization3 Role:Role3</li>
-        <li>Name:Organization4 Role:Role4</li>
-        <li>Name:Organization5 Role:Role5</li>
+      <ul >
+        <li className={styles.listOrg}>Name:Organization1 Role:Role1</li>
+        <li className={styles.listOrg}>Name:Organization2 Role:Role2</li>
+        <li className={styles.listOrg}>Name:Organization3 Role:Role3</li>
+        <li className={styles.listOrg}>Name:Organization4 Role:Role4</li>
+        <li className={styles.listOrg}>Name:Organization5 Role:Role5</li>
       </ul>
     </section>
   )
