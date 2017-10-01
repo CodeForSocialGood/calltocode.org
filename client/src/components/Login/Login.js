2f3f797268466a8d5cc1b10d3ddea5edaf17eb7c
Ryan Thomas <ryanthomas970@gmail.com>
2017-09-30 23:33:14 -0700

    Fix Login component input display issues.

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -5,9 +5,9 @@ function Login () {
   return (
     <form className={styles.form}>
       <h1 className={styles.title}>Login</h1>
-      <input className={styles.inputEmail} type="text" placeholder="Email" />
-      <input className={styles.inputPassword} type="password" placeholder="Password" />
-      <input className={styles.buttonSubmit} type="submit" value="Login" />
+      <div className={styles.inputRow}><input className={styles.inputEmail} type="text" placeholder="Email" /></div>
+      <div className={styles.inputRow}><input className={styles.inputPassword} type="password" placeholder="Password" /></div>
+      <div className={styles.inputRow}><input className={styles.buttonSubmit} type="submit" value="Login" /></div>
     </form>
   )
 }
