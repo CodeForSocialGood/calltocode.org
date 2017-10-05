5f232a8dc60599fbff7c7e08aae7a076d0a8f7bd
Ryan Thomas <ryanthomas970@gmail.com>
2017-10-04 21:25:01 -0700

    Remove logging for redux form changes.
    Previously, redux was logging every change in the form inputs to the console, which meant a new console log every character that was typed. This has been removed.

--- client/src/index.js
+++ client/src/index.js
@@ -8,7 +8,7 @@ import { Provider } from 'react-redux'
 
 import createHistory from 'history/createBrowserHistory'
 
-import { ConnectedRouter, routerReducer, routerMiddleware, push } from 'react-router-redux'
+import { ConnectedRouter, routerReducer, routerMiddleware } from 'react-router-redux'
 
 import reducers from './reducers'
 
@@ -18,16 +18,18 @@ const history = createHistory()
 // Build the middleware for intercepting and dispatching navigation actions
 const middleware = routerMiddleware(history)
 
-// const logger = createLogger()
-
-// const createStoreWithMiddleware = applyMiddleware(logger)(createStore)
+const logger = createLogger({
+  // predicate: (getState, action) => !action.type.contains('redux-form')
+  predicate: (getState, action) => action.type.indexOf('redux-form') === -1
+})
 
 const store = createStore(
   combineReducers({
     ...reducers,
     router: routerReducer
   }),
-  applyMiddleware(middleware)
+  applyMiddleware(middleware),
+  applyMiddleware(logger)
 )
 
 ReactDOM.render(
