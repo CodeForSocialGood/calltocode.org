8b4d9a05c8558e3beb064d0864734cab7c6298d3
Ryan Thomas <ryanthomas970@gmail.com>
2017-10-05 20:35:30 -0700

    Feature/login kevin (#33)
    * docs: correct instructions on updating branch to match upstream.
    
    * feat: Route login button to login page.
    
      + Add login and sign up buttons.
    
    [#151007470]
    
    * Add sample username and password in a json file for testing.
    
    * Set up basics for react-redux.
    
    The app looks and operates the same as before but now there is scaffolding for implementing redux.
    
    * feat: Implement logging in using a debug button that will be removed once the Login component is fixed.
    
     + Add redux-logging so that developers can debug/track state more easily during development
    
    [#151007470]
    
    * Fix Login component input display issues.
    
    * feat: Update Login form to send the appropriate Redux action with the login information.
    
     + Added redux-form
    
    [#151007470]
    
    * Delete Unnecessary Layout Element class and replaced it with a simple <div>
    
    * Refactor Routes to use the Switch component instead of exact paths.
    
    * Refactor the code to include react-redux-router
    
    + Modify .babelrc to deal with spread operator
    
    * refactor: Extract anonymous function for creating password input into a function on the component's class
    
    * Make page redirect to home page after user logs in successfully.
    
    Now code just needs to be cleaned up before being merged with master.
    
    [#151007470]
    
    * Remove logging for redux form changes.
    
    Previously, redux was logging every change in the form inputs to the console, which meant a new console log every character that was typed. This has been removed.
    
    * Made form validate inputs based off the provided credentials
    
    Previously the form actually redirected regardless of whether or not the right credentials were supplied. However, the user wouldn't be logged in if the credentials were wrong.
    Now the user is only redirected if the credentials are correct.
    
     + Remove some console logs that were being used for debugging
     + Add PropType validation for props that were brought in from redux-form and redux in the Login component
    
    [#151007470]
    
    * feat: Completed the Kevin logs in User Story
    
    [#151007470]
    
    * Add a wildcard route in order to avoid getting errors from the server side due to react routes that express didn't know about.
    
    [#151007470]
    
    * refactor: Refactor index.js to include better names for middleware and remove comments.
    
    [#151007470]
    
    * refactor: Rename validate function to be more readable so that comment can be removed
    
    [#151007470]
    
    * Change the way loggedIn state was represented in redux.
    
    [#151007470]
    
    * Change the way sourcemaps are generated back from 'eval' to 'sourcemap' in webpack.
    
    [#151007470]
    
    * Add default state to loginReducer
    
    [#151007470]
    
    * Remove login from props to Header
    
    [#151007470]
    
    * Update routing to use the RouterService
    
    [#151007470]
    
    * Remove unnecessary path variable
    
    [#151007470]

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,39 @@
 import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
+import App from './App'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { createLogger } from 'redux-logger'
+import { Provider } from 'react-redux'
 
-// import Login from './components/Login/Login'
-import Signup from './components/Signup/Signup'
-//import ListOfProjects from './components/ListOfProjects/ListOfProjects'
+import createHistory from 'history/createBrowserHistory'
 
-// ReactDOM.render(<Login />, document.querySelector('main'))
-//ReactDOM.render(<ListOfProjects />, document.querySelector('main'))
-ReactDOM.render(<Signup />, document.querySelector('main'))
+import { ConnectedRouter, routerReducer, routerMiddleware } from 'react-router-redux'
+
+import reducers from './reducers'
+
+const browserHistory = createHistory()
+
+const navigationMiddleware = routerMiddleware(browserHistory)
+
+const loggedMiddleware = createLogger({
+  predicate: (getState, action) => !action.type.includes('redux-form')
+})
+
+const store = createStore(
+  combineReducers({
+    ...reducers,
+    router: routerReducer
+  }),
+  applyMiddleware(navigationMiddleware),
+  applyMiddleware(loggedMiddleware)
+)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={browserHistory}>
+      <App />
+    </ConnectedRouter>
+  </Provider>
+  , document.querySelector('main')
+)
