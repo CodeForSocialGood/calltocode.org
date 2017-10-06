57eab15c416c871e25dfe3122d7d6563b9f49b4f
Karthi Jagatheesan <karthijey@yahoo.com>
2017-10-05 20:02:43 -0400

    Signup form 151008020 (#31)
    * Signup Form added
    
    * signup form modified with minor changes

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,11 @@
 import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
+
 // import Login from './components/Login/Login'
-import ListOfProjects from './components/ListOfProjects/ListOfProjects'
+import Signup from './components/Signup/Signup'
+//import ListOfProjects from './components/ListOfProjects/ListOfProjects'
 
 // ReactDOM.render(<Login />, document.querySelector('main'))
-ReactDOM.render(<ListOfProjects/>, document.querySelector('main'))
+//ReactDOM.render(<ListOfProjects />, document.querySelector('main'))
+ReactDOM.render(<Signup />, document.querySelector('main'))
