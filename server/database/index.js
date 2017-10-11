a72623e9caee8fb56ab83f491898bfeca6ba102d
Joe Lee <blueberrymozart@gmail.com>
2017-10-11 07:16:21 -0500

    test(fix): fix linting issue

--- server/database/index.js
+++ server/database/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 function connectToDatabase () {
   const dbUser = process.env.DB_USER || 'user'
-  cosnt dbPass = process.env.DB_PASS || 'pass'
+  const dbPass = process.env.DB_PASS || 'pass'
 
   mongoose.connect(`mongodb://${dbUser}:${dbPass}@ds117495.mlab.com:17495/c2c`, {
     useMongoClient: true
