c6d247c98a7e705f0f619dce4ca4732a4826c4f7
mattwills8 <matt_wills8@hotmail.co.uk>
2017-10-30 23:03:20 -0500

    add forgot password link

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch } from 'react-router-dom'
 import Header from './components/Header/Header'
 import LoginForm from './components/LoginForm/LoginForm'
 import SignupForm from './components/SignupForm/SignupForm'
+import ForgotPasswordForm from './components/ForgotPasswordForm/ForgotPasswordForm'
 import ListOfProjects from './components/ListOfProjects/ListOfProjects'
 
 function App () {
@@ -13,6 +14,7 @@ function App () {
       <Switch>
         <Route path='/login' component={LoginForm} />
         <Route path='/signup' component={SignupForm} />
+        <Route path='/forgot-password' component={ForgotPasswordForm} />
         <Route path='/' component={ListOfProjects} />
       </Switch>
     </div>
