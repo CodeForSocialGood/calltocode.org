9ed4f8085d9e123e1b7bb6b63ab610a1a6bfb127
Matt Wills <matt_wills8@hotmail.co.uk>
2017-11-02 14:59:33 -0500

    Feature/forgot password form (#59)
    * add forgot password link
    
    * add forgot password form component and link to /forgot-password route
    
    * move form styles to reusable scss, add styles to forgot password form
    
    * fix linting error - remove SubmissionError from import

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,19 @@ import {Route, Switch} from 'react-router-dom'
 import Header from './components/Header/Header'
 import LoginForm from './components/LoginForm/LoginForm'
 import SignupForm from './components/SignupForm/SignupForm'
+import ForgotPasswordForm from './components/ForgotPasswordForm/ForgotPasswordForm'
 import ListOfProjects from './components/ListOfProjects/ListOfProjects'
 
-function App() {
+function App () {
   return (
     <div>
       <Header />
-        <Switch>
-          <Route path='/login' component={LoginForm}/>
-          <Route path='/signup' component={SignupForm}/>
-          <Route path='/' component={ListOfProjects}/>
-        </Switch>
+      <Switch>
+        <Route path='/login' component={LoginForm} />
+        <Route path='/signup' component={SignupForm} />
+        <Route path='/forgot-password' component={ForgotPasswordForm} />
+        <Route path='/' component={ListOfProjects} />
+      </Switch>
     </div>
   )
 }
