5d7f52fd0efb3c5557e41203aed2aac8d5db706b
Mongolialady <hhy615@gmail.com>
2017-11-02 23:39:06 -0700

    save before continue

--- client/src/components/ListOfProjects/ListOfProjects.js
+++ client/src/components/ListOfProjects/ListOfProjects.js
@@ -1,20 +1,21 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 
 import styles from './ListOfProjects.scss'
 import projects from '../../data/projects.json'
 import emailApiClient from '../../api/email'
 
-import { connect } from 'react-redux'
+import {connect} from 'react-redux'
 
 class ListOfProjects extends Component {
-  constructor (props) {
-    super(props)
+  constructor(props) {
+    super(props);
 
     this.renderListOfProjects = this.renderListOfProjects.bind(this)
+    this.renderProject = this.renderProject.bind(this);
   }
 
-  renderListOfProjects () {
+  renderListOfProjects() {
     const liClassName = this.props.loggedIn ? styles.listOrgLoggedIn : styles.listOrg
 
     return projects.map((project, index) => {
@@ -23,13 +24,32 @@ class ListOfProjects extends Component {
           key={index}
           className={liClassName}
           onClick={this.props.loggedIn ? mailToOrganization(project) : null}>
-          Name:{project.name} Role:{project.role}
+          Name:{project.name} Role:{project.role} {this.renderProject(project)}
         </li>
       )
     })
   }
 
-  render () {
+
+  renderProject(project) {
+    if (true === project.applicationStatus) {
+      return (
+        <span className={styles.projectApplicationPassed}>
+          &#10004;
+        </span>
+      )
+    } else if (false === project.applicationStatus) {
+      return (
+        <span className={styles.projectApplicationFailed}>
+          &#10007;
+        </span>
+      )
+    } else {
+      return null;
+    }
+  }
+
+  render() {
     return (
       <section className={styles.orgSection}>
         <h1 className={styles.title}>Apply Below</h1>
@@ -41,7 +61,7 @@ class ListOfProjects extends Component {
   }
 }
 
-function mailToOrganization (project) {
+function mailToOrganization(project) {
   return () => {
     const {email, name, role} = project
     const projectInfo = {email, name, role}
@@ -50,8 +70,8 @@ function mailToOrganization (project) {
   }
 }
 
-function mapStateToProps (state) {
-  return { loggedIn: state.login.loggedIn }
+function mapStateToProps(state) {
+  return {loggedIn: state.login.loggedIn}
 }
 
 ListOfProjects.propTypes = {
