3523562417362e82b4954b0b7e7369918d8a5157
Mongolialady <hhy615@gmail.com>
2017-11-03 22:56:41 -0700

    save before continue

--- client/src/components/ListOfProjects/ListOfProjects.js
+++ client/src/components/ListOfProjects/ListOfProjects.js
@@ -1,36 +1,55 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 
 import styles from './ListOfProjects.scss'
-import defaultProjects from '../../data/projects.json'
+import projects from '../../data/projects.json'
 import emailApiClient from '../../api/email'
 
-import { connect } from 'react-redux'
+import {connect} from 'react-redux'
 
 class ListOfProjects extends Component {
-  constructor (props) {
-    super(props)
+  constructor(props) {
+    super(props);
 
     this.renderListOfProjects = this.renderListOfProjects.bind(this)
-    this.projects = this.props.projects || defaultProjects
+    this.renderProject = this.renderProject.bind(this);
   }
 
-  renderListOfProjects () {
+  renderListOfProjects() {
     const liClassName = this.props.loggedIn ? styles.listOrgLoggedIn : styles.listOrg
 
-    return this.projects.map((project, index) => {
+    return projects.map((project, index) => {
       return (
         <li
           key={index}
           className={liClassName}
           onClick={this.props.loggedIn ? mailToOrganization(project) : null}>
-          Name:{project.name} Role:{project.role}
+          Name:{project.name} Role:{project.role} {this.renderProject(project)}
         </li>
       )
     })
   }
 
-  render () {
+
+  renderProject(project) {
+    if (true === project.applicationStatus) {
+      return (
+        <span className={styles.projectApplicationPassed}>
+          &#10004;
+        </span>
+      )
+    } else if (false === project.applicationStatus) {
+      return (
+        <span className={styles.projectApplicationFailed}>
+          &#10007;
+        </span>
+      )
+    } else {
+      return null;
+    }
+  }
+
+  render() {
     return (
       <section className={styles.orgSection}>
         <h1 className={styles.title}>Apply Below</h1>
@@ -40,24 +59,26 @@ class ListOfProjects extends Component {
       </section>
     )
   }
+
 }
 
-function mailToOrganization (project) {
+
+
+function mailToOrganization(project) {
   return () => {
-    const {email, name, role} = project
-    const projectInfo = {email, name, role}
+    const {email, name, role} = project;
+    const projectInfo = {email, name, role};
 
     emailApiClient.send(projectInfo)
   }
 }
 
-function mapStateToProps (state) {
-  return { loggedIn: state.login.loggedIn }
+function mapStateToProps(state) {
+  return {loggedIn: state.login.loggedIn}
 }
 
 ListOfProjects.propTypes = {
-  loggedIn: PropTypes.bool.isRequired,
-  projects: PropTypes.array
-}
+  loggedIn: PropTypes.bool.isRequired
+};
 
 export default connect(mapStateToProps)(ListOfProjects)
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,9 @@
 import loginReducer from './loginReducer'
 import { reducer as formReducer } from 'redux-form'
+import projectReducer from "./projectReducer"
 
 export default {
   login: loginReducer,
-  form: formReducer
+  form: formReducer,
+  projects:projectReducer
 }
