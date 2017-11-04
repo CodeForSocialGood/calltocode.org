d00618c09487d8ddc4f3c40e44da174e6a5e05d7
Mongolialady <hhy615@gmail.com>
2017-11-03 23:11:31 -0700

    save before continue

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,23 @@
 import React from 'react'
-import { Route, Switch } from 'react-router-dom'
-
+import {Route, Switch} from 'react-router-dom'
+import {Provider} from "react-redux";
 import Header from './components/Header/Header'
 import LoginForm from './components/LoginForm/LoginForm'
 import SignupForm from './components/SignupForm/SignupForm'
 import ListOfProjects from './components/ListOfProjects/ListOfProjects'
+import store from "./data/store";
 
-function App () {
+function App() {
   return (
     <div>
       <Header />
-      <Switch>
-        <Route path='/login' component={LoginForm} />
-        <Route path='/signup' component={SignupForm} />
-        <Route path='/' component={ListOfProjects} />
-      </Switch>
+      <Provider store={store}>
+        <Switch>
+          <Route path='/login' component={LoginForm}/>
+          <Route path='/signup' component={SignupForm}/>
+          <Route path='/' component={ListOfProjects}/>
+        </Switch>
+      </Provider>
     </div>
   )
 }
