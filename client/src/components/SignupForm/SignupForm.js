a28d790b56118d16571689572b973cc241c5e364
linkin-park <giridharan.arun123@gmail.com>
2017-11-07 05:23:29 +0530

    Feature/#151789249 (#63)
    * Added ag to .git-authors
    
    * removed the span , popup shows even after the password is valid and only hides when its out of focus for the field

--- client/src/components/SignupForm/SignupForm.js
+++ client/src/components/SignupForm/SignupForm.js
@@ -28,7 +28,6 @@ function PasswordField ({ input, meta: { error, active } }) {
       type="password"
       placeholder="Password"
       {...input} />,
-    <span key="valid" className={error ? styles.cross : styles.tick}></span>,
     <Field key="popup" name="popup"
       component={ValidationPopup} active={active} error={error} />
   ]
@@ -36,16 +35,16 @@ function PasswordField ({ input, meta: { error, active } }) {
 
 const ValidationPopup = ({error, active}) => {
   return (
-    <div className={`${styles.validpopup} ${active && error ? styles.show : styles.hide}` }>
+    <div className={`${styles.validpopup} ${active  ? styles.show : styles.hide}` }>
       <p className={styles.bold}>Password must have</p>
       <ul>
-        <li className={ error && error.upperCase ? styles.tick : styles.cross }> at least 1 UpperCase Character </li>
-        <li className={ error && error.lowerCase ? styles.tick : styles.cross }> at least 1 LowerCase Character </li>
-        <li className={ error && error.hasOneDigit ? styles.tick : styles.cross }> at least 1 Number </li>
-        <li className={ error && error.hasSpecialChar ? styles.tick : styles.cross }> at least 1 Special Character </li>
-        <li className={ error && error.minLength ? styles.tick : styles.cross }> at least 10 Characters </li>
-        <li className={ error && error.maxLength ? styles.tick : styles.cross }> at most 128 Characters </li>
-        <li className={ error && error.noIdenticalChars ? styles.tick : styles.cross }> not more than 2 identical characters in a row </li>
+        <li className={ error == null || error.upperCase ? styles.tick : styles.cross }> at least 1 UpperCase Character </li>
+        <li className={ error == null || error.lowerCase  ? styles.tick : styles.cross }> at least 1 LowerCase Character </li>
+        <li className={ error == null || error.hasOneDigit  ? styles.tick : styles.cross }> at least 1 Number </li>
+        <li className={ error == null || error.hasSpecialChar ? styles.tick : styles.cross }> at least 1 Special Character </li>
+        <li className={ error == null || error.minLength ? styles.tick : styles.cross }> at least 10 Characters </li>
+        <li className={ error == null || error.maxLength ? styles.tick : styles.cross }> at most 128 Characters </li>
+        <li className={ error == null || error.noIdenticalChars ? styles.tick : styles.cross }> not more than 2 identical characters in a row </li>
       </ul>
     </div>
   )
