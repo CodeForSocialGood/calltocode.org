7f9bb65e2f1d1f5b042ce9c8df288d3047b71c3c
Mongolialady <hhy615@gmail.com>
2017-11-09 12:39:22 -0800

    save before continue

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,19 @@
 import React from 'react'
-import { Route, Switch } from 'react-router-dom'
-
+import {Route, Switch} from 'react-router-dom'
 import Header from './components/Header/Header'
 import LoginForm from './components/LoginForm/LoginForm'
 import SignupForm from './components/SignupForm/SignupForm'
-import ForgotPasswordForm from './components/ForgotPasswordForm/ForgotPasswordForm'
 import ListOfProjects from './components/ListOfProjects/ListOfProjects'
-import Profile from './components/Profile/Profile'
-import restricted from './components/Restricted/Restricted'
 
-function App () {
+function App() {
   return (
     <div>
       <Header />
-      <Switch>
-        <Route path='/login' component={LoginForm} />
-        <Route path='/signup' component={SignupForm} />
-        <Route path='/profile' component={restricted(Profile)} />
-        <Route path='/forgot-password' component={ForgotPasswordForm} />
-        <Route path='/' component={ListOfProjects} />
-      </Switch>
+        <Switch>
+          <Route path='/login' component={LoginForm}/>
+          <Route path='/signup' component={SignupForm}/>
+          <Route path='/' component={ListOfProjects}/>
+        </Switch>
     </div>
   )
 }
