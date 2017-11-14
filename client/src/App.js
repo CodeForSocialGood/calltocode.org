08c0b0b04de2320b1513abf2c1ef478ec08600b6
Mongolialady <hhy615@gmail.com>
2017-11-14 13:08:16 -0800

    upload my changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,28 @@
 import React from 'react'
-import { Route, Switch } from 'react-router-dom'
+import {Route, Switch} from 'react-router-dom'
 
 import Header from './components/Header/Header'
 import LoginForm from './components/LoginForm/LoginForm'
 import SignupForm from './components/SignupForm/SignupForm'
 import ForgotPasswordForm from './components/ForgotPasswordForm/ForgotPasswordForm'
 import ListOfProjects from './components/ListOfProjects/ListOfProjects'
+import { Provider } from 'react-redux'
 import Profile from './components/Profile/Profile'
 import restricted from './components/Restricted/Restricted'
 
-function App () {
+function App() {
   return (
     <div>
       <Header />
-      <Switch>
-        <Route path='/login' component={LoginForm} />
-        <Route path='/signup' component={SignupForm} />
-        <Route path='/profile' component={restricted(Profile)} />
-        <Route path='/forgot-password' component={ForgotPasswordForm} />
-        <Route path='/' component={ListOfProjects} />
-      </Switch>
+      <Provider store={store}>
+        <Switch>
+          <Route path='/login' component={LoginForm}/>
+          <Route path='/signup' component={SignupForm}/>
+          <Route path='/profile' component={restricted(Profile)}/>
+          <Route path='/forgot-password' component={ForgotPasswordForm}/>
+          <Route path='/' component={ListOfProjects}/>
+        </Switch>
+      </Provider>
     </div>
   )
 }
