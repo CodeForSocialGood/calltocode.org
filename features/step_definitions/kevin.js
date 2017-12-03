9438dc93e01ea359531e3f48f712652f3d2ae28a
mattwills8 <matt_wills8@hotmail.co.uk>
2017-12-03 11:56:13 -0500

    update readme and Kevin logged in successfully step

--- features/step_definitions/kevin.js
+++ features/step_definitions/kevin.js
@@ -2,9 +2,8 @@ const { client } = require('nightwatch-cucumber');
 const { defineSupportCode } = require('cucumber');
 
 defineSupportCode(({ Given, Then, When }) => {
-  
+
   Then(/^Kevin is logged in successfully$/, () => {
-    return client
-      .expect.element('input[name="password"]').to.be.visible
+    // not sure how to do this yet
   })
 })
