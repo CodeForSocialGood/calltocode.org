0cb90c2225d0506f1e3cfa8b7c8f4ac9615aac89
Tim Davis <timdavish@gmail.com>
2017-12-07 14:37:39 -0800

    refactor(db): opportunity -> project

--- deploy/run.sh
+++ deploy/run.sh
@@ -21,7 +21,7 @@ start () {
     docker-compose rm -vf
     docker-compose up -d db
     docker-compose exec db mongoimport --db admin --collection users --file /seedData/users.json
-    docker-compose exec db mongoimport --db admin --collection opportunities --file /seedData/opportunities.json
+    docker-compose exec db mongoimport --db admin --collection projects --file /seedData/projects.json
     docker-compose up -d app
   popd
 }
