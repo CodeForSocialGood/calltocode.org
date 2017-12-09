c1cf8e0ed249f1152f5722b6f6aafc2089a19adb
Sérgio Monteiro <sexta.feira.13@gmail.com>
2017-12-09 19:56:38 +0000

    increate time to 1h - > 3600secs

--- server/database/models/ForgotPassword.js
+++ server/database/models/ForgotPassword.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const ForgotPasswordSchema = mongoose.Schema({
   code: String,
   email: String,
-  createdAt: { type: Date, default: Date.now, expires: '60' }
+  createdAt: { type: Date, default: Date.now, expires: '3600' }
 })
 
 const ForgotPassword = mongoose.model('ForgotPassword', ForgotPasswordSchema)
