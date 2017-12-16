8da0966087ca5a7e309f3e8df3bc9f0aa488489d
Tim Davis <timdavish@gmail.com>
2017-12-15 16:38:41 -0800

    chore(db): update mongoimport and seed data
    - update seed data to jsonArray format
    - update mongoimport command in db/run.sh to use jsonArray format
    - this allows for the reuse of the seed data in server controller tests

--- db/run.sh
+++ db/run.sh
@@ -12,5 +12,6 @@ docker run --name c4sg -d -p 27017:27017 -p 28017:28017 mongo
 echo "Adding seed data to MongoDB.."
 docker cp ./db/seedData/users.json c4sg:users.json
 docker cp ./db/seedData/projects.json c4sg:projects.json
-docker exec c4sg mongoimport --db admin --collection users --file users.json
-docker exec c4sg mongoimport --db admin --collection projects --file projects.json
+
+docker exec c4sg mongoimport --db admin --collection users --file users.json --type json --jsonArray
+docker exec c4sg mongoimport --db admin --collection projects --file projects.json --type json --jsonArray
