4e289ac046a0eb8b3fdfe4e8febe8b64b2fbf8fb
Tim Davis <timdavish@gmail.com>
2017-12-18 18:05:16 -0800

    Chore/#153725201 - Fix server logger (#100)
    * chore(server): update server logger
    
    - Logging is determined by two rollbar config factors: enabled and
    verbose
    - If enabled, logging is forwarded to rollbar. verbose is then used by rollbar to determine
    if the console logs what rollbar logs
    - If disabled, verbose determines if the console does the logging
    - If we wanted no logging at all in an environment, both enabled and
    verbose would need to be set to false
    
    * chore(server): update rollbar config
    
    * chore(server): refactor server logger
    
    * chore(server): pass all arguments to rollbar
    
    - rollbar's log functions parse all parameters in a way such that order
    doesn't matter, making it okay to just pass all arguments to it. this
    way we can successfully log errors like `logger.error('Database
    connection error', error)`

--- server/logger.js
+++ server/logger.js
@@ -4,14 +4,22 @@ const { rollbarConfig } = require('./config')
 const rollbar = new Rollbar(rollbarConfig)
 
 const logger = {
-  // Forward these errors to rollbar, verbose option decides to log locally
-  critical (error) { rollbar.critical(error) },
-  error (error) { rollbar.error(error) },
-  warn (warning) { rollbar.warning(warning) },
-  // These will never report to rollbar, manually check verbose option
-  info (...args) { if (rollbarConfig.verbose) { console.info.apply(console, args) } },
-  debug (...args) { if (rollbarConfig.verbose) { console.log.apply(console, args) } },
-  log (...args) { this.debug.apply(this.debug, args) }
+  critical: handleLog('critical', 'error'),
+  error: handleLog('error'),
+  warn: handleLog('warn'),
+  info: handleLog('info'),
+  debug: handleLog('debug'),
+  log: handleLog('log')
+}
+
+function handleLog (rollbarLevel = 'debug', localLevel = rollbarLevel) {
+  return function () {
+    if (rollbarConfig.enabled) {
+      rollbar[rollbarLevel](...arguments)
+    } else if (rollbarConfig.verbose) {
+      console[localLevel](...arguments)
+    }
+  }
 }
 
 module.exports = logger
