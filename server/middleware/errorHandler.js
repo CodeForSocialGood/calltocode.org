fc0600e187d2e50f62b22558272f063eb2803a2c
Tim Davis <timdavish@gmail.com>
2018-01-07 14:43:54 -0800

    chore(server): remove logging while running tests

--- server/middleware/errorHandler.js
+++ server/middleware/errorHandler.js
@@ -2,7 +2,9 @@ const logger = require('../logger')
 
 function errorHandler () {
   return function (err, req, res, next) {
-    logger.error(err)
+    if (!process.env.SILENT) {
+      logger.error(err)
+    }
 
     const error = { name: err.name, message: err.message, stack: err.stack }
     for (const prop in err) error[prop] = err[prop]
