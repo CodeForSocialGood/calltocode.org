862d88db00fef5c24d73cdaf8b6690c43d6ab4cd
mattwills8 <matt_wills8@hotmail.co.uk>
2018-01-07 17:15:33 +0000

    rebase master

--- client/test/e2e/page_objects/header.js
+++ client/test/e2e/page_objects/header.js
@@ -1,8 +1,8 @@
 module.exports = {
   elements: {
+    homeButton: 'a[href="/"]',
     signupButton: 'a[href="/signup"]',
     loginButton: 'a[href="/login"]',
-    logoutButton: 'a[href="/"]',
     profileButton: 'a[href="/profile"]',
     createProjectButton: 'a[href="/create-project"]'
   }
--- features/step_definitions/kevin.js
+++
@@ -1,9 +0,0 @@
-const { client } = require('nightwatch-cucumber');
-const { defineSupportCode } = require('cucumber');
-
-defineSupportCode(({ Given, Then, When }) => {
-
-  Then(/^Kevin is logged in successfully$/, () => {
-    // not sure how to do this yet
-  })
-})
