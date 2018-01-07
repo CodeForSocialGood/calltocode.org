15c0ecd1328acc4ff88c2c124d5413f05b7984e0
Tim Davis <timdavish@gmail.com>
2018-01-07 14:49:20 -0800

    chore(server): move silent check to logger

--- server/middleware/errorHandler.js
+++ server/middleware/errorHandler.js
@@ -2,9 +2,7 @@ const logger = require('../logger')
 
 function errorHandler () {
   return function (err, req, res, next) {
-    if (!process.env.SILENT) {
-      logger.error(err)
-    }
+    logger.error(err)
 
     const error = { name: err.name, message: err.message, stack: err.stack }
     for (const prop in err) error[prop] = err[prop]
