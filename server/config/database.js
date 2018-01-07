be9e7e7745ae497ec1618e16f76704a4fa1be8a7
Joe Lee <blueberrymozart@gmail.com>
2018-01-07 03:57:21 -0600

    chore: hook up with mongodb atlas for test and prod

--- server/config/database.js
+++ server/config/database.js
@@ -1,9 +1,11 @@
 const config = {
   get url () {
     switch (process.env.NODE_ENV) {
-      case 'prod': return `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ds117495.mlab.com:17495/c2c`
-      case 'test': return 'mongodb://admin@db:27017/admin'
-      default: return `mongodb://admin@${process.env.DB_URL}:27017/admin`
+      case 'test': return `mongodb://admin:${process.env.DB_PASS}` +
+        '@calltocode-shard-00-00-romiu.mongodb.net:27017,calltocode-shard-00-01-romiu.mongodb.net:27017,calltocode-shard-00-02-romiu.mongodb.net:27017/test?ssl=true&replicaSet=calltocode-shard-0&authSource=admin'
+      case 'prod': return `mongodb://admin:${process.env.DB_PASS}` +
+        '@calltocode-shard-00-00-o20xv.mongodb.net:27017,calltocode-shard-00-01-o20xv.mongodb.net:27017,calltocode-shard-00-02-o20xv.mongodb.net:27017/test?ssl=true&replicaSet=calltocode-shard-0&authSource=admin'
+      default: return `mongodb://admin@127.0.0.1:27017/admin`
     }
   }
 }
