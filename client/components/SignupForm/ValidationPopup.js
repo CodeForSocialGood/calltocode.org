acdcda69e685f5d25d0a8f89ed3599709c741d16
Julian Katz <juliankatz1991@gmail.com>
2018-01-09 20:52:59 -0700

    Refactored component a bit to clean up redundant conditions

--- client/components/SignupForm/ValidationPopup.js
+++ client/components/SignupForm/ValidationPopup.js
@@ -3,18 +3,23 @@ import PropTypes from 'prop-types'
 
 import styles from './SignupForm.scss'
 
+const getTickOrCross = (error, errorKey) => {
+  const isError = error === null || error[errorKey]
+  return isError ? styles.tick : styles.cross
+}
+
 const ValidationPopup = ({ error, active }) => {
   return (
     <div className={`${styles.validationPopup} ${active ? styles.show : styles.hide}` }>
       <p className={styles.bold}>Password must have</p>
       <ul>
-        <li className={ error === null || error.upperCase ? styles.tick : styles.cross }> at least 1 UpperCase Character </li>
-        <li className={ error === null || error.lowerCase ? styles.tick : styles.cross }> at least 1 LowerCase Character </li>
-        <li className={ error === null || error.hasOneDigit ? styles.tick : styles.cross }> at least 1 Number </li>
-        <li className={ error === null || error.hasSpecialChar ? styles.tick : styles.cross }> at least 1 Special Character </li>
-        <li className={ error === null || error.minLength ? styles.tick : styles.cross }> at least 10 Characters </li>
-        <li className={ error === null || error.maxLength ? styles.tick : styles.cross }> at most 128 Characters </li>
-        <li className={ error === null || error.noIdenticalChars ? styles.tick : styles.cross }> not more than 2 identical characters in a row </li>
+        <li className={ getTickOrCross(error, 'upperCase') }> at least 1 UpperCase Character </li>
+        <li className={ getTickOrCross(error, 'lowerCase') }> at least 1 LowerCase Character </li>
+        <li className={ getTickOrCross(error, 'hasOneDigit') }> at least 1 Number </li>
+        <li className={ getTickOrCross(error, 'hasSpecialChar') }> at least 1 Special Character </li>
+        <li className={ getTickOrCross(error, 'minLength') }> at least 10 Characters </li>
+        <li className={ getTickOrCross(error, 'maxLength') }> at most 128 Characters </li>
+        <li className={ getTickOrCross(error, 'noIdenticalChars') }> not more than 2 identical characters in a row </li>
       </ul>
     </div>
   )
