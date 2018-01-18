8f3a3696b21f54ff57e50c21d081b4f244b2112e
Joe Lee <blueberrymozart@gmail.com>
2018-01-17 18:29:14 -0600

    chore: refactor send verification email logic

--- server/routes/controllers/forgotPasswordController.js
+++ server/routes/controllers/forgotPasswordController.js
@@ -16,60 +16,54 @@ const forgotPasswordController = {
     return this
   },
 
-  _saveIntoDatabase (email, code) {
-    const query = {'email': email}
-    // upsert is to insert or update the record. this way, only one code per email is in DB
-    return this.ForgotPass.findOneAndUpdate(query, {$set: { email, code }}, {upsert: true, 'new': true}).exec()
+  _insertOrUpdateVerificationCode (email, code) {
+    return this.ForgotPass.findOneAndUpdate({email}, {$set: { email, code }}, {upsert: true, 'new': true}).exec()
   },
 
-  _generateCode () {
-    return Math.floor(Math.random() * 900000) + 100000 // generate a 6 digit code
+  _generateSixDigitCode () {
+    return Math.floor(Math.random() * 900000) + 100000
   },
 
-  _getUser (email) {
-    return this.Users.findOne({email}).exec((err, forgot) => {
-      return !!err
-    })
+  _checkIfUserExists (email) {
+    return this.Users.findOne({email}).exec()
   },
 
-  sendVerificationCodeEmail (req, res) {
+  async sendVerificationCodeEmail (req, res) {
     const email = req.body.email
 
-    this._getUser(email).then((exists) => {
-      // if the user does not exist, do not generate code and send email. It's expected that the user inserts a valid email.
-      if (!exists) {
-        return res.status(200).send({ status: 200 })
+    try {
+      await this._checkIfUserExists(email)
+      const code = this._generateSixDigitCode()
+      await this._insertOrUpdateVerificationCode(email, code)
+
+      const text = `Your validation code is: ${code}`
+      const message = {
+        from: 'team.calltocode@gmail.com',
+        to: email,
+        subject: `Validation code for ${email}`,
+        html: `<strong>${text}</strong>`,
+        text
       }
-      const code = this._generateCode()
-      this._saveIntoDatabase(email, code)
-        .then(saveForgot => {
-          const text = `Your validation code is: ${code}`
-          const message = {
-            from: 'team.calltocode@gmail.com',
-            to: email,
-            subject: `Validation code for ${email}`,
-            html: `<strong>${text}</strong>`,
-            text
-          }
-          this.emailClient.send(message)
-          return res.status(200).send({ status: 200 })
-        })
-        .catch(error => { if (error) return res.status(500).send({ status: 500 }) })
-    }).catch(error => { if (error) return res.status(500).send({ status: 500 }) })
+      this.emailClient.send(message)
+
+      res.status(200).send({ status: 200 })
+    } catch (error) {
+      res.status(500).send({ status: 500 })
+    }
   },
 
   validateCode (req, res) {
     const {email, code} = req.body
-    this.ForgotPass.findOne({email, code}).exec((err, forgot) => {
-      if (err) {
-        return res.status(404).json({error: 'Invalid code or email'})
-      }
-      if (forgot) {
-        forgot.remove()
-        return res.status(200).json({status: 200})
-      }
-      return res.status(404).json({error: 'Invalid code or email'})
-    })
+
+    this.ForgotPass.findOne({email, code}).exec()
+      .then(user => {
+        if (user) {
+          user.remove()
+          return res.status(200).json({status: 200})
+        }
+        res.status(404).json({error: 'Invalid code or email'})
+      })
+      .catch(() => res.status(404).json({error: 'Invalid code or email'}))
   }
 }
 
