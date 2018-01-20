2e97da16334cb7776ec370d2f7b1346a02c3ef8e
David Becker <david@beckersite.com>
2018-01-20 10:42:44 -0700

    Add tests for NewPasswordForm

--- client/components/NewPasswordForm/NewPasswordForm.test.js
+++ client/components/NewPasswordForm/NewPasswordForm.test.js
@@ -15,20 +15,31 @@ beforeEach(() => {
 
 describe('<NewPasswordForm />', () => {
   describe('email exists in database', () => {
-    
-    describe('password of length 0', () => {
+    beforeEach(() => { mockProps.email = 'kevin@email.com' })
 
+    test('component renders correctly', () => {
+      const component = shallow(<NewPasswordForm {...mockProps} />)
+      expect(component).toMatchSnapshot()
     })
-    describe('password not of length 0', () => {
-
-    })
+    // describe('password of length 0', () => {
+    //   beforeEach(() => { mockProps})
+    // })
+    // describe('password not of length 0', () => {
+    //
+    // })
   })
   describe('email does not exist in database', () => {
-    describe('password of length 0', () => {
+    beforeEach(() => { mockProps.email = 'email@email.com' })
 
+    test('component renders correctly', () => {
+      const component = shallow(<NewPasswordForm {...mockProps} />)
+      expect(component).toMatchSnapshot()
     })
-    describe('password not of length 0', () => {
-
-    })
+    // describe('password of length 0', () => {
+    //
+    // })
+    // describe('password not of length 0', () => {
+    //
+    // })
   })
 })
