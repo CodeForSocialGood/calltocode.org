6a5fceefbf1f2692b33f449a70144f3c9fe6e19d
Joe Lee <blueberrymozart@gmail.com>
2018-01-24 13:53:40 -0600

    chore: kill db ports as part of pre e2e script

--- e2e/pree2e.sh
+++ e2e/pree2e.sh
@@ -5,8 +5,12 @@ set -x
 # kill app
 APP_PID=$(lsof -t -i:3000)
 APP_WATCH_PID=$(lsof -t -i:3001)
+DB_PID=$(lsof -t -i:27017)
+DB_ALT_PID=$(lsof -t -i:28017)
 if [ -n $APP_PID ]; then kill $APP_PID; fi
 if [ -n $APP_WATCH_PID ]; then kill $APP_WATCH_PID; fi
+if [ -n $DB_PID ]; then kill $DB_PID; fi
+if [ -n $DB_ALT_PID ]; then kill $DB_ALT_PID; fi
 
 set -e
 
