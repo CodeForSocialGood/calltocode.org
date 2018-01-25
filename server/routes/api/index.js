6c2d51ab6533565577bd5083ba99fc3b88ee4a03
Tim Davis <timdavish@gmail.com>
2018-01-25 08:03:32 -0800

    chore(server): update projects api & controller
    - use express-promise-router
    - use async/await
    - update integration tests

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -4,8 +4,16 @@ import emailRoutes from './email'
 import forgotPasswordRoutes from './forgotPassword'
 import projectsRoutes from './projects'
 import usersRoutes from './users'
+import { simulateDelay, simulateError } from '../../lib/simulate'
 
 const router = express.Router()
+
+// Add a simulated delay (ms) or error chance (%) to api requests
+const API_REQUEST_DELAY = 0
+const API_REQUEST_ERROR_CHANCE = 0
+
+router.use(simulateDelay(API_REQUEST_DELAY))
+router.use(simulateError(API_REQUEST_ERROR_CHANCE))
 
 router.use('/email', emailRoutes)
 router.use('/forgot-password', forgotPasswordRoutes)
--- server/routes/api/projects.js
+++ server/routes/api/projects.js
@@ -1,18 +1,19 @@
-import express from 'express'
+import expressRouter from 'express-promise-router'
 
 import auth from '../../lib/middleware/auth'
 import _projects from '../controllers/projects'
 
-const router = express.Router()
+const router = expressRouter()
 const projects = _projects._init()
 
 router.route('/')
   .get(auth.optional, projects.getProjects)
-  // TODO: below, this should be `auth.required`. client api first needs to use
-  // `apiRequest` with `apiOptionsFromState` in order to pass authentication.
   .post(auth.optional, projects.createProject)
 
-router.route('/:project')
+router.route('/:projectId')
   .get(auth.optional, projects.getProject)
+  .put(auth.optional, projects.putProject)
+
+router.param('projectId', projects.projectById)
 
 export default router
