2c8df8c21cb69a4a1fdce66ab5237f6d52a3ffa1
Mongolialady <hhy615@gmail.com>
2018-01-25 00:58:09 -0800

    save before continue

--- client/reducers/loginReducer.js
+++ client/reducers/loginReducer.js
@@ -1,3 +1,21 @@
 /**
  * Created by hhy615 on 1/25/18.
  */
+
+import { DISABLE_LOGIN, ENABLE_LOGIN } from '../actions/header/types'
+import initialState from '/initialState'
+
+
+export default function( state = initialState.login, action ){
+  const {type} = action;
+
+  switch(type){
+    case DISABLE_LOGIN:
+      return { ...state, enable : false };
+    case ENABLE_LOGIN:
+      return { ...state, enable : true };
+
+    default:
+      return state;
+  }
+}
