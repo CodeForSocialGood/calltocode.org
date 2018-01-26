7fded2f9eeb2bbe5d47b3ee07a47dcbe912bb33d
Tim Davis <timdavish@gmail.com>
2018-01-26 08:57:11 -0800

    chore(server): update users tests

--- server/test/integration/users.test.js
+++ server/test/integration/users.test.js
@@ -71,7 +71,7 @@ test.serial('getUsers', async t => {
   t.is(res.body.length, users.length)
 })
 
-test.serial.skip('signup, valid user', async t => {
+test.serial('signup, valid user', async t => {
   const { app } = t.context
   const user = {
     usertype: 'volunteer',
