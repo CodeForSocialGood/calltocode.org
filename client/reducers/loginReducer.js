ff79e1729217c5f9b5c8ee64ab16019ef46d7f12
Mongolialady <hhy615@gmail.com>
2018-01-26 16:04:40 -0800

    enable / disable login button

--- client/reducers/loginReducer.js
+++ client/reducers/loginReducer.js
@@ -5,17 +5,16 @@
 import { DISABLE_LOGIN, ENABLE_LOGIN } from '../actions/header/types'
 import initialState from './initialState'
 
+export default function (state = initialState.login, action) {
+  const {type} = action
 
-export default function( state = initialState.login, action ){
-  const {type} = action;
-
-  switch(type){
+  switch (type) {
     case DISABLE_LOGIN:
-      return { ...state, enable : false };
+      return { ...state, enable: false }
     case ENABLE_LOGIN:
-      return { ...state, enable : true };
+      return { ...state, enable: true }
 
     default:
-      return state;
+      return state
   }
 }
