67d2f76717715bbf0874e9272816dc190ac1b424
Tim Davis <timdavish@gmail.com>
2018-01-28 10:44:06 -0800

    chore(e2e): try to fix build

--- .setup/client/clear-ports.sh
+++ .setup/client/clear-ports.sh
@@ -1,8 +1,23 @@
 #!/usr/bin/env bash
 
-set +x -e
+set +x
 
 # Kill the app
-lsof -ti tcp:3000,3001 | xargs kill
+APP_PORT=3000
+APP_WATCH_PORT=3001
+APP_PID=$(lsof -ti tcp:$APP_PORT)
+APP_WATCH_PID=$(lsof -ti tcp:$APP_WATCH_PORT)
 
-echo "--- Cleared ports 3000 and 3001"
+set -e
+
+# Make sure there is something to kill
+if [[ ! -z $APP_PID ]] ; then
+  kill -9 $APP_PID
+fi
+
+# Make sure there is something to kill
+if [[ ! -z $APP_WATCH_PID ]] ; then
+  kill -9 $APP_WATCH_PID
+fi
+
+echo "--- Cleared ports $APP_PORT and $APP_WATCH_PORT"
