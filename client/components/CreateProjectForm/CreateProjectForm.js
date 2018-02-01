2f1064ed017f2846d61edc3baff7e1b8a525fcc8
Tim Davis <timdavish@gmail.com>
2018-02-01 14:12:08 -0800

    feature(client): send project causes in createProject api request

--- client/components/CreateProjectForm/CreateProjectForm.js
+++ client/components/CreateProjectForm/CreateProjectForm.js
@@ -31,7 +31,8 @@ class CreateProjectForm extends Component {
     this.renderCauses = this.renderCauses.bind(this)
     this.state = {
       error: '',
-      projectName: ''
+      projectName: '',
+      causes: []
     }
   }
   saveFile () {
@@ -44,7 +45,7 @@ class CreateProjectForm extends Component {
   }
 
   async createProject () {
-    const response = await projectsApiClient.createProject(this.state.projectName, this.props.user.organization)
+    const response = await projectsApiClient.createProject(this.state.projectName, this.state.causes, this.props.user.organization)
     if (response.status === 500) {
       this.setState({error: response.statusText})
     }
