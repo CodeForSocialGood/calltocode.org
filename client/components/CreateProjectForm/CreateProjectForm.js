400a0cafc73a195624f08263af193b15e524cbdf
Tim Davis <timdavish@gmail.com>
2018-02-01 13:50:58 -0800

    feature(client): CreateProjectForm causes

--- client/components/CreateProjectForm/CreateProjectForm.js
+++ client/components/CreateProjectForm/CreateProjectForm.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react'
-import UploadDropzone from '../UploadDropzone/UploadDropzone'
-import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
-import styles from './CreateProjectForm.scss'
-import projectsApiClient from '../../api/projects'
-
-import { buttonSubmit } from './createProjectFormJss'
+import { connect } from 'react-redux'
 import { withStyles } from 'material-ui/styles'
 import TextField from 'material-ui/TextField/TextField'
 import Button from 'material-ui/Button/Button'
+import Checkbox from 'material-ui/Checkbox'
+import { FormGroup, FormLabel, FormControlLabel } from 'material-ui/Form'
+
+import projectsApiClient from '../../api/projects'
+import UploadDropzone from '../UploadDropzone/UploadDropzone'
+import styles from './CreateProjectForm.scss'
+import { buttonSubmit } from './createProjectFormJss'
+
+const causes = [
+  'Animal',
+  'Environment',
+  'International NGO',
+  'Health',
+  'Education',
+  'Arts & Culture',
+  'Other'
+]
 
 class CreateProjectForm extends Component {
   constructor (props, context) {
@@ -16,6 +28,7 @@ class CreateProjectForm extends Component {
     this.createProject = this.createProject.bind(this)
     this.saveFile = this.saveFile.bind(this)
     this.projectNameChange = this.projectNameChange.bind(this)
+    this.renderCauses = this.renderCauses.bind(this)
     this.state = {
       error: '',
       projectName: ''
@@ -37,6 +50,17 @@ class CreateProjectForm extends Component {
     }
   }
 
+  renderCauses () {
+    return causes.map((cause, index) => {
+      return (
+        <FormControlLabel key={index}
+          control={ <Checkbox value="checkedA" /> }
+          label={cause}
+        />
+      )
+    })
+  }
+
   render () {
     const {classes} = this.props
     return (
@@ -44,12 +68,17 @@ class CreateProjectForm extends Component {
         <h1 className={styles.formHeading}>Create New Project</h1>
 
         <UploadDropzone className={styles.inputImageUpload}
-          saveFile={() => {}}/>
+          saveFile={this.saveFile} />
 
         <TextField className={styles.inputProjectName}
           required id="projectname"
           label="Project name" onChange={this.projectNameChange}
           type="text" fullWidth name="projectname"/>
+
+        <FormLabel>Causes</FormLabel>
+        <FormGroup row>
+          {this.renderCauses()}
+        </FormGroup>
 
         <Button type="submit" disabled={this.state.projectName.length === 0} raised className={classes.root} color="primary" fullWidth>
         Create Project
