330cbc399d24993aa5455c512c447df22aa18302
Tim Davis <timdavish@gmail.com>
2018-02-14 14:44:49 -0800

    chore(client): export constants on their own as well

--- client/components/shared/constants/causes.js
+++ client/components/shared/constants/causes.js
@@ -1,9 +1,16 @@
+export const ANIMAL = 'Animal'
+export const ENVIRONMENT = 'Environment'
+export const INTERNATIONAL_NGO = 'International NGO'
+export const HEALTH = 'Health'
+export const EDUCATION = 'Education'
+export const ARTS_CULTURE = 'Arts & Culture'
+
 export default [
-  'Animal',
-  'Environment',
-  'International NGO',
-  'Health',
-  'Education',
-  'Arts & Culture',
+  ANIMAL,
+  ENVIRONMENT,
+  INTERNATIONAL_NGO,
+  HEALTH,
+  EDUCATION,
+  ARTS_CULTURE,
   'Other'
 ]
