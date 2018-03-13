ec2f80be022d7385b3cdcc7d6c273b8a53ecf055
goodgoof <anjalidsharma8@gmail.com>
2018-03-12 21:59:44 -0400

    feature #155111485- Visitor sees landing page(Section F)

--- client/components/LandingF/LandingF.js
+++ client/components/LandingF/LandingF.js
@@ -29,3 +29,28 @@ class Footer extends React.Component {
 }
 
 export default Footer
+            <li className={styles.company}>Company</li>
+            <li className={styles.list-items styles.press}><a href="#">Press</a></li>
+            <li className={styles.list-items}><a href="#">Releases</li>
+            <li className={styles.list-items}><a href="#">Mission</li>
+            <li className={styles.list-items}><a href="#">Strategy</li>
+            <li className={styles.list-items}><a href="#">Works</li>
+          </ul>
+        </div>
+
+        <div className={styles.column3}>
+          <p className={styles.FOLLOW-US}>FOLLOW US</p>
+          <div className={styles.social-media}>
+            <a href="#"><img src={facebook} alt="facebook logo" className="facebook"></a>
+            <a href="#"><img src={twitter} alt="twitter logo" className="twitter"></a>
+            <a href="#"><img src={linked-in} alt="linkedin logo" className="linked-in"></a>
+            <a href="#"><img src={pininterest} alt="pininterest logo" className="pininterest"></a>
+        </div>
+        </div>
+      </div>
+    )
+  }
+};
+
+
+export default LandingF
