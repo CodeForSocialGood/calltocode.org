3b157570d0589cf118bb3235e84f6d116cd0810f
goodgoof <anjalidsharma8@gmail.com>
2018-03-13 14:24:26 -0400

    commented out the images

--- client/components/LandingF/LandingF.js
+++ client/components/LandingF/LandingF.js
@@ -1,12 +1,12 @@
 
 import React, { Component } from 'react'
 import styles from './LandingF.scss'
-import logo from '../../images/logo-home.png'
-import facebook from '../../images/facebook.png'
-import linked-in from '../../images/linked-in.png'
-import pininterest from '../../images/pininterest.png'
-import twitter from '../../images/twitter.png'
-import 501-c-3-nonprofit from '../../images/501-c-3-nonprofit.png'
+// import logo from '../../images/logo-home.png'
+// import facebook from '../../images/facebook.png'
+// import linked-in from '../../images/linked-in.png'
+// import pininterest from '../../images/pininterest.png'
+// import twitter from '../../images/twitter.png'
+// import 501-c-3-nonprofit from '../../images/501-c-3-nonprofit.png'
 
 
 class Footer extends React.Component {
@@ -14,38 +14,38 @@ class Footer extends React.Component {
     return (
       <div className={styles.container}>
         <div className={styles.column1}>
-          <img src={logo} alt="Call to code logo" className="logo-home">
-          <p><img src={501-c-3-nonprofit} className="non-profit"></p>
+          // <img src={logo} alt="Call to code logo" className="logo-home">
+          <p><img src={501-c-3-nonprofit} className="non-profit" /></p>
         </div>
 
 
         <div className={styles.column2}>
           <ul className={styles.list}>
             <li className={styles.product}>Product</li>
-            <li className={styles.list-items styles.popular}><a href="#">Popular</li>
-            <li className={styles.list-items}><a href="#">Trending</li>
-            <li className={styles.list-items}><a href="#">Catalog</li>
+            <li className={styles.list-items styles.popular}>Popular</li>
+            <li className={styles.list-items}>Trending</li>
+            <li className={styles.list-items}>Catalog</li>
           </ul>
         </div>
 
         <div className={styles.column3}>
           <ul className={styles.list}>
             <li className={styles.company}>Company</li>
-            <li className={styles.list-items styles.press}><a href="#">Press</a></li>
-            <li className={styles.list-items}><a href="#">Releases</li>
-            <li className={styles.list-items}><a href="#">Mission</li>
-            <li className={styles.list-items}><a href="#">Strategy</li>
-            <li className={styles.list-items}><a href="#">Works</li>
+            <li className={styles.list-items styles.press}>Press</a></li>
+            <li className={styles.list-items}>Releases</li>
+            <li className={styles.list-items}>Mission</li>
+            <li className={styles.list-items}>Strategy</li>
+            <li className={styles.list-items}>Works</li>
           </ul>
         </div>
 
         <div className={styles.column3}>
           <p className={styles.FOLLOW-US}>FOLLOW US</p>
           <div className={styles.social-media}>
-            <a href="#"><img src={facebook} alt="facebook logo" className="facebook"></a>
-            <a href="#"><img src={twitter} alt="twitter logo" className="twitter"></a>
-            <a href="#"><img src={linked-in} alt="linkedin logo" className="linked-in"></a>
-            <a href="#"><img src={pininterest} alt="pininterest logo" className="pininterest"></a>
+            // <a href="#"><img src={facebook} alt="facebook logo" className="facebook"></a>
+            // <a href="#"><img src={twitter} alt="twitter logo" className="twitter"></a>
+            // <a href="#"><img src={linked-in} alt="linkedin logo" className="linked-in"></a>
+            // <a href="#"><img src={pininterest} alt="pininterest logo" className="pininterest"></a>
         </div>
         </div>
       </div>
