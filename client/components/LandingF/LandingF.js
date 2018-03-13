811b9d1cff8be91945ce490f01f2a726797e0844
goodgoof <anjalidsharma8@gmail.com>
2018-03-13 14:12:33 -0400

    remove first 2 lines

--- client/components/LandingF/LandingF.js
+++ client/components/LandingF/LandingF.js
@@ -1,5 +1,4 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+
 import React, { Component } from 'react'
 import styles from './LandingF.scss'
 import logo from '../../images/logo-home.png'
