c0045de835061d2b50f2431c9a04f8ffddafeaab
goodgoof <anjalidsharma8@gmail.com>
2018-03-13 14:35:21 -0400

    put images back with closing tag

--- client/components/LandingF/LandingF.js
+++ client/components/LandingF/LandingF.js
@@ -24,12 +24,12 @@ import ReactDOM from 'react-dom'
 >>>>>>> remove first 2 lines
 import React, { Component } from 'react'
 import styles from './LandingF.scss'
-// import logo from '../../images/logo-home.png'
-// import facebook from '../../images/facebook.png'
-// import linked-in from '../../images/linked-in.png'
-// import pininterest from '../../images/pininterest.png'
-// import twitter from '../../images/twitter.png'
-// import 501-c-3-nonprofit from '../../images/501-c-3-nonprofit.png'
+import logo from '../../images/logo-home.png'
+import facebook from '../../images/facebook.png'
+import linked-in from '../../images/linked-in.png'
+import pininterest from '../../images/pininterest.png'
+import twitter from '../../images/twitter.png'
+import 501-c-3-nonprofit from '../../images/501-c-3-nonprofit.png'
 
 
 class Footer extends React.Component {
@@ -37,7 +37,7 @@ class Footer extends React.Component {
     return (
       <div className={styles.container}>
         <div className={styles.column1}>
-          // <img src={logo} alt="Call to code logo" className="logo-home">
+          <img src={logo} alt="Call to code logo" className="logo-home" />
           <p><img src={501-c-3-nonprofit} className="non-profit" /></p>
         </div>
 
@@ -83,10 +83,10 @@ export default Footer
         <div className={styles.column3}>
           <p className={styles.FOLLOW-US}>FOLLOW US</p>
           <div className={styles.social-media}>
-            // <a href="#"><img src={facebook} alt="facebook logo" className="facebook"></a>
-            // <a href="#"><img src={twitter} alt="twitter logo" className="twitter"></a>
-            // <a href="#"><img src={linked-in} alt="linkedin logo" className="linked-in"></a>
-            // <a href="#"><img src={pininterest} alt="pininterest logo" className="pininterest"></a>
+            <a href="#"><img src={facebook} alt="facebook logo" className="facebook" /></a>
+            <a href="#"><img src={twitter} alt="twitter logo" className="twitter" /></a>
+            <a href="#"><img src={linked-in} alt="linkedin logo" className="linked-in" /></a>
+            <a href="#"><img src={pininterest} alt="pininterest logo" className="pininterest" /></a>
         </div>
         </div>
       </div>
