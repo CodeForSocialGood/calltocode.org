1ef28c06a27c921153edc28569c4b87f421a7e4f
goodgoof <anjalidsharma8@gmail.com>
2018-03-14 10:18:23 -0400

    changed class names

--- client/components/LandingF/LandingF.js
+++ client/components/LandingF/LandingF.js
@@ -3,10 +3,10 @@ import React, { Component } from 'react'
 import styles from './LandingF.scss'
 import logo from '../../images/logo-home.png'
 import facebook from '../../images/facebook.png'
-import linked-in from '../../images/linked-in.png'
+import linkedin from '../../images/linked-in.png'
 import pininterest from '../../images/pininterest.png'
 import twitter from '../../images/twitter.png'
-import 501-c-3-nonprofit from '../../images/501-c-3-nonprofit.png'
+import c3nonprofit from '../../images/501-c-3-nonprofit.png'
 
 
 class Footer extends React.Component {
@@ -15,36 +15,36 @@ class Footer extends React.Component {
       <div className={styles.container}>
         <div className={styles.column1}>
           <img src={logo} alt="Call to code logo" className="logo-home" />
-          <p><img src={501-c-3-nonprofit} className="non-profit" /></p>
+          <p><img src={c3nonprofit} className="non-profit" /></p>
         </div>
 
 
         <div className={styles.column2}>
           <ul className={styles.list}>
             <li className={styles.product}>Product</li>
-            <li className={styles.list-items styles.popular}>Popular</li>
-            <li className={styles.list-items}>Trending</li>
-            <li className={styles.list-items}>Catalog</li>
+            <li className={styles.listItems}>Popular</li>
+            <li className={styles.listItems}>Trending</li>
+            <li className={styles.listItems}>Catalog</li>
           </ul>
         </div>
 
         <div className={styles.column3}>
           <ul className={styles.list}>
             <li className={styles.company}>Company</li>
-            <li className={styles.list-items styles.press}>Press</a></li>
-            <li className={styles.list-items}>Releases</li>
-            <li className={styles.list-items}>Mission</li>
-            <li className={styles.list-items}>Strategy</li>
-            <li className={styles.list-items}>Works</li>
+            <li className={styles.listItems}>Press</a></li>
+            <li className={styles.listItems}>Releases</li>
+            <li className={styles.listItems}>Mission</li>
+            <li className={styles.listItems}>Strategy</li>
+            <li className={styles.listItems}>Works</li>
           </ul>
         </div>
 
-        <div className={styles.column3}>
-          <p className={styles.FOLLOW-US}>FOLLOW US</p>
-          <div className={styles.social-media}>
+        <div className={styles.column4}>
+          <p className={styles.followUS}>FOLLOW US</p>
+          <div className={styles.socialMedia}>
             <a href="#"><img src={facebook} alt="facebook logo" className="facebook" /></a>
             <a href="#"><img src={twitter} alt="twitter logo" className="twitter" /></a>
-            <a href="#"><img src={linked-in} alt="linkedin logo" className="linked-in" /></a>
+            <a href="#"><img src={linked-in} alt="linkedin logo" className="linkedIn" /></a>
             <a href="#"><img src={pininterest} alt="pininterest logo" className="pininterest" /></a>
         </div>
         </div>
