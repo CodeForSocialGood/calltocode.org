4b043e983c400411b95b81dd0fda926481edad6f
goodgoof <anjalidsharma8@gmail.com>
2018-03-15 14:37:34 -0400

    fixing test bugs

--- client/components/LandingF/LandingF.js
+++ client/components/LandingF/LandingF.js
@@ -1,24 +1,18 @@
 
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import styles from './LandingF.scss';
-import logoV from '../../images/logo-home-video-copy.png';
-import facebook from '../../images/facebook.png';
-import linkedIn from '../../images/linked-in.png';
-import pininterest from '../../images/pininterest.png';
-import twitter from '../../images/twitter.png';
-import c3nonprofit from '../../images/501-c-3-nonprofit.png';
-
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import styles from './LandingF.scss'
+import logoV from '../../images/logo-home-video-copy.png'
+import c3nonprofit from '../../images/501-c-3-nonprofit.png'
 
 class Footer extends React.Component {
-  render(){
+  render() {
     return (
       <div className={styles.container}>
         <div className={styles.column1}>
           <img src={logoV} alt="Call to code logo"/>
           <p><img src={c3nonprofit} className={styles.nonProfit} /></p>
         </div>
-
 
         <div className={styles.column2}>
           <ul className={styles.list}>
@@ -31,7 +25,6 @@ class Footer extends React.Component {
             <li className={styles.about}><a href="#" className={styles.link}>About</a></li>
           </ul>
         </div>
-
       </div>
     )
   }
