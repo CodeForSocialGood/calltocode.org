effb094ee51300aec4b9f66448efda0fa4d099d6
goodgoof <anjalidsharma8@gmail.com>
2018-03-15 14:37:34 -0400

    fixing test bugs

--- client/components/LandingF/LandingF.js
+++ client/components/LandingF/LandingF.js
@@ -79,7 +79,6 @@ class Footer extends React.Component {
             <li className={styles.about}><a href="/about" className={styles.link}>About</a></li>
           </ul>
         </div>
-
       </div>
     )
   }
