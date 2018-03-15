e44b256662d56dfb478a52d36c1f4e2a4fc0864d
goodgoof <anjalidsharma8@gmail.com>
2018-03-15 12:37:10 -0400

    changed logo image

--- client/components/LandingF/LandingF.js
+++ client/components/LandingF/LandingF.js
@@ -1,7 +1,21 @@
+<<<<<<< HEAD
 import React from 'react'
 import styles from './LandingF.scss'
 import logoV from '../../images/logo-home-video-copy.png'
 import c3nonprofit from '../../images/501-c-3-nonprofit.png'
+=======
+
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import styles from './LandingF.scss';
+import logoV from '../../images/logo-home-video-copy.png';
+import facebook from '../../images/facebook.png';
+import linkedIn from '../../images/linked-in.png';
+import pininterest from '../../images/pininterest.png';
+import twitter from '../../images/twitter.png';
+import c3nonprofit from '../../images/501-c-3-nonprofit.png';
+
+>>>>>>> changed logo image
 
 class Footer extends React.Component {
   render () {
@@ -9,7 +23,11 @@ class Footer extends React.Component {
       <div className={styles.container}>
         <div className={styles.column1}>
           <img src={logoV} alt="Call to code logo"/>
+<<<<<<< HEAD
           <p><img src={c3nonprofit} className={styles.nonProfit} /></p>
+=======
+          <p><img src={c3nonprofit} className="nonProfit" /></p>
+>>>>>>> changed logo image
         </div>
 
         <div className={styles.column2}>
