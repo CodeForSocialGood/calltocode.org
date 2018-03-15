530c82df48beba4ad93126c284d68ed123af0f73
goodgoof <anjalidsharma8@gmail.com>
2018-03-15 10:19:20 -0400

    removed few links

--- client/components/LandingF/LandingF.js
+++ client/components/LandingF/LandingF.js
@@ -56,6 +56,11 @@ class Footer extends React.Component {
             <li className={styles.listItems}>Popular</li>
             <li className={styles.listItems}>Trending</li>
             <li className={styles.listItems}>Catalog</li>
+            <li className={styles.projects}>Projects</li>
+            // <li className={styles.listItems}>Popular</li>
+            // <li className={styles.listItems}>Trending</li>
+            // <li className={styles.listItems}>Catalog</li>
+            // <li className={styles.listItems}>Features</li>
           </ul>
         </div>
 
@@ -76,18 +81,24 @@ export default Footer
             <li className={styles.listItems}>Mission</li>
             <li className={styles.listItems}>Strategy</li>
             <li className={styles.listItems}>Works</li>
+            <li className={styles.about}>About</li>
+            // <li className={styles.listItems}>Press</li>
+            // <li className={styles.listItems}>Releases</li>
+            // <li className={styles.listItems}>Mission</li>
+            // <li className={styles.listItems}>Strategy</li>
+            // <li className={styles.listItems}>Works</li>
           </ul>
         </div>
 
-        <div className={styles.column4}>
-          <p className={styles.followUS}>FOLLOW US</p>
-          <div className={styles.socialMedia}>
-            <a href="#"><img src={facebook} alt="facebook logo" className={styles.facebook} /></a>
-            <a href="#"><img src={twitter} alt="twitter logo" className={styles.twitter} /></a>
-            <a href="#"><img src={linkedIn} alt="linkedin logo" className={styles.linkedIn} /></a>
-            <a href="#"><img src={pininterest} alt="pininterest logo" className={styles.pininterest} /></a>
-        </div>
-        </div>
+        // <div className={styles.column4}>
+        //   <p className={styles.followUS}>FOLLOW US</p>
+        //   <div className={styles.socialMedia}>
+        //     <a href="#"><img src={facebook} alt="facebook logo" className={styles.facebook} /></a>
+        //     <a href="#"><img src={twitter} alt="twitter logo" className={styles.twitter} /></a>
+        //     <a href="#"><img src={linkedIn} alt="linkedin logo" className={styles.linkedIn} /></a>
+        //     <a href="#"><img src={pininterest} alt="pininterest logo" className={styles.pininterest} /></a>
+        // </div>
+        // </div>
       </div>
     )
   }
