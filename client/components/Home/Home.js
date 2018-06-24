597bacca1c10450249a2ba869d0882d3ad9ee4d4
Mongolialady <hhy615@gmail.com>
2018-06-24 13:18:02 -0700

    Combine six landing page sections into a single Lander component and route "/" to it.

--- client/components/Home/Home.js
+++ client/components/Home/Home.js
@@ -16,9 +16,9 @@ import LandingF from '../LandingF/LandingF'
 
 class Home extends Component {
   constructor (props) {
-    super(props);
-    this.handleCheckbox = this.handleCheckbox.bind(this);
-    this.renderList = this.renderList.bind(this);
+    super(props)
+    this.handleCheckbox = this.handleCheckbox.bind(this)
+    this.renderList = this.renderList.bind(this)
     this.state = {
       filters: {
         causes: [],
@@ -36,7 +36,7 @@ class Home extends Component {
     if (this.props.projects !== nextProps.projects) {
       this.setState({filteredProjects: nextProps.projects})
     }
-  };
+  }
 
   projectContainsCause (project) {
     return this.state.filters.causes.length === 0 || this.state.filters.causes.some(cause => project.causes.indexOf(cause) >= 0)
@@ -46,17 +46,17 @@ class Home extends Component {
   }
 
   handleCheckbox (event, checked) {
-    const filters = this.state.filters;
-    const index = this.state.filters[event.target.name].indexOf(event.target.value);
+    const filters = this.state.filters
+    const index = this.state.filters[event.target.name].indexOf(event.target.value)
     if (index === -1) {
       filters[event.target.name].push(event.target.value)
     } else {
       filters[event.target.name].splice(index, 1)
     }
-    this.setState({ filters });
+    this.setState({ filters })
     const filteredProjects = this.props.projects.filter(project => {
       return this.projectContainsCause(project) && this.projectContainsTech(project)
-    });
+    })
     this.setState({ filteredProjects })
   }
 
@@ -99,12 +99,12 @@ function mapStateToProps (state) {
 const mapDispatchToProps = {
   onLoad: ProjectActionCreator.fetchAllProjects
 
-};
+}
 
 Home.propTypes = {
   onLoad: PropTypes.func,
   projects: PropTypes.array
 
-};
+}
 
 export default connect(mapStateToProps, mapDispatchToProps)(Home)
